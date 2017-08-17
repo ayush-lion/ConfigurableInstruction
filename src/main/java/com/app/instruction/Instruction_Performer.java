e509c8703be6f88e7ee280313c5a7a061cee30e9
panwar67 <shubhampanwar67@yahoo.com>
2017-08-17 18:08:37 +0530

    pojo retry

---
+++ src/main/java/com/app/instruction/Instruction_Performer.java
@@ -0,0 +1,13 @@
+package com.app.instruction;
+
+
+//class for drawing on instruction panel 
+public class Instruction_Performer 
+{
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/app/instruction/Provider.java
@@ -0,0 +1,67 @@
+package com.app.instruction;
+
+import java.awt.Image;
+
+public class Provider {
+
+	//class for configuring drawing and performing instruction callous.
+	
+	
+	String Provider_Allotee;
+	String Provider_Image;
+	String[] Provider_Text;
+	String Provider_Shape;
+	String Provider_Max_Lines;
+	
+	public Provider()
+	{
+		
+	}
+	
+	public void setProvider_Allotee(String provider_Allotee) {
+		this.Provider_Allotee = provider_Allotee;
+	}
+	
+	public void setProvider_Image(String provider_Image) {
+		this.Provider_Image = provider_Image;
+	}
+	
+	public void setProvider_Max_Lines(String provider_Max_Lines) {
+		this.Provider_Max_Lines = provider_Max_Lines;
+	}
+	
+	public void setProvider_Shape(String provider_Shape) {
+		this.Provider_Shape = provider_Shape;
+	}
+	
+	public void setProvider_Text(String[] provider_Text) {
+		this.Provider_Text = provider_Text;
+	}
+	
+	public String getProvider_Allotee() {
+		return this.Provider_Allotee;
+	}
+	
+	public String getProvider_Image() {
+		return this.Provider_Image;
+	}
+	
+	public String getProvider_Max_Lines() {
+		return this.Provider_Max_Lines;
+		
+	}
+	
+	public String getProvider_Shape() {
+		return this.Provider_Shape;
+	}
+	
+	public String[] getProvider_Text() {
+		return this.Provider_Text;
+	}
+	
+
+	
+	
+	
+	
+}
